b9fed01c84b5bfa2dd6aaa4f5527d8b3a42cc3a2
Dennis <dennis.drochmann@gmail.com>
2025-02-09 17:29:34 +0100

    update

--- start.bat
+++ start.bat
@@ -6,14 +6,14 @@ SET PROJECT_PATH=%~dp0
 REM Wechsel in das richtige Verzeichnis
 cd /d "%PROJECT_PATH%"
 
-REM Prüfe, ob Electron installiert ist
+REM Prüfe, ob Electron verfügbar ist (npx nutzt globale Installation)
 where electron >nul 2>nul
-IF %ERRORLEVEL% NEQ 0 (
+IF ERRORLEVEL 1 (
     echo Electron nicht gefunden, versuche zu installieren...
-    npm install -g electron
+    npm install -g electron || exit /b 1
 )
 
-REM Starte Electron mit main.js
+REM Starte Electron mit main.js über npx (nutzt globale Installation, falls vorhanden)
 npx electron main.js
 
 REM Pause für Windows
